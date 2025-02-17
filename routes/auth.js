c364178e0da61bd1848f2e12e25c0d1d8f389873
Lilian Napoleon Modupe <napoleonliliantari@outlook.com>
2025-02-17 21:54:20 +0100

    feat: updated validator code

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const { signup, signin } = require("../controllers/authController");
 const { signupValidationRules, signinValidationRules, validate } = require("../validators/authValidators");
 
-// Apply validation rules and middleware
+// Applied validation rules and middleware
 router.post("/signup", signupValidationRules, validate, signup);
 router.post("/signin", signinValidationRules, validate, signin);
 
